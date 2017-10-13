597c0e9ce9ab417520446c0c2f02b884819a1010
TwoWieners <rybronk@yahoo.com>
2017-10-13 17:37:57 -0400

    Started on the Ninja
    Character Base Class set up
    Ninja Class Set up
    Started On Ninja Animations and Input for them

---
+++ Assets/Scripts/BaseCharacter_Script.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BaseCharacter_Script : MonoBehaviour {
+
+    public struct Weapon
+    {
+        float mRange, mRateofFire, mProjectileSpeed;
+        GameObject 
+            mWeapon, //The Weapon and it's animation if needed
+            mProjectile; //Projectile for the Weapon and it's animation
+
+    }
+
+    public ushort mHealth, mDamage;
+    public uint mMovementSpeed;
+
+    public Weapon mWeapon;
+    protected Animator mAnimator; //The Different Animations for the different movements
+    public GameObject[] mHitboxes; //Depending on the character, they'll have different hitboxes
+
+    // Use this for initialization
+    virtual public void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	virtual public void Update () {
+		
+	}
+
+    virtual public void JumpAction()
+    {
+
+    }
+    virtual public void AttackAction()
+    {
+
+    }
+    virtual public void SlidingAction()
+    {
+
+    }
+    virtual public void RunAction()
+    {
+
+    }
+    virtual public void CrouchAction()
+    {
+
+    }
+    virtual public void DeathAction()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Characters/Char_Ninja_Script.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char_Ninja_Script : BaseCharacter_Script {
+
+	// Use this for initialization
+	override public void Start () {
+        mAnimator = GetComponent<Animator>();
+	}
+	// Update is called once per frame
+	override public void Update () {
+        RunAction();
+	}
+    override public void JumpAction()
+    {
+
+    }
+    override public void AttackAction()
+    {
+
+    }
+    override public void SlidingAction()
+    {
+
+    }
+    override public void RunAction()
+    {
+        if (Input.GetKey(KeyCode.A)) // Going Left
+        { 
+
+            mAnimator.SetBool("LeftDownPress", true);
+            if(transform.localEulerAngles.y != 180)
+                transform.localEulerAngles = new Vector3(0, 180, 0);
+        }
+        else
+            if (mAnimator.GetBool("LeftDownPress"))
+                mAnimator.SetBool("LeftDownPress", false);
+
+        if (Input.GetKey(KeyCode.D)) // Going Right
+        {
+            mAnimator.SetBool("RightDownPress", true);
+            if(transform.localEulerAngles.y != 0)
+                transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+            if (mAnimator.GetBool("RightDownPress"))
+                mAnimator.SetBool("RightDownPress", false);
+
+        if (mAnimator.GetBool("LeftDownPress") && mAnimator.GetBool("RightDownPress"))//Pressing Both Buttons // Goes Idle
+        {
+            mAnimator.SetBool("LeftDownPress", false);
+            mAnimator.SetBool("RightDownPress", false);
+        }
+    }
+    override public void CrouchAction()
+    {
+
+    }
+    override public void DeathAction()
+    {
+
+    }
+}
